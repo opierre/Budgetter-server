94caedcf82f8fc4295f5cb1035f7bbe89f29dd7f
opierre <pierre.o@hotmail.fr>
2025-12-04 22:57:52 +0100

    refactor: remove urls test

--- budgetter_server/dashboard/urls.py
+++ budgetter_server/dashboard/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -10,7 +9,4 @@ router.register('account', views.AccountViewSet)
 router.register('category', views.CategoryViewSet)
 router.register('transaction', views.TransactionViewSet)
 
-urlpatterns = [
-    path('import/', views.import_ofx, name='import_ofx'),
-    path('import/preview/', views.preview_import, name='preview_import'),
-] + router.urls
+urlpatterns = router.urls
